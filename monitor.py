24b5ac6a2cc75c09cbbe21cb8023c667d9947dcb
shabuddinshaik <mr.shabuddinshaik@gmail.com>
2024-09-23 16:05:34 +0530

    modified monitor.py to use postgres URL

--- monitor.py
+++ monitor.py
@@ -15,19 +15,25 @@ POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "your_db")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "your_user")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "your_password")
-
-IDLE_THRESHOLD = "1 hour"
+IDLE_THRESHOLD = os.getenv("IDLE_THRESHOLD", "1 hour")
 
 def terminate_idle_connections():
     try:
-        logging.info("Connecting to PostgreSQL database...")
-        conn = psycopg2.connect(POSTGRES_URL) if POSTGRES_URL else psycopg2.connect(
-            dbname=POSTGRES_DB,
-            user=POSTGRES_USER,
-            password=POSTGRES_PASSWORD,
-            host=POSTGRES_HOST,
-            port=POSTGRES_PORT
-        )
+        logging.info(f"POSTGRES_URL: {POSTGRES_URL}")
+
+        if POSTGRES_URL:
+            logging.info("Using POSTGRES_URL for connection")
+            conn = psycopg2.connect(POSTGRES_URL)
+        else:
+            logging.info(f"Connecting using host: {POSTGRES_HOST}, port: {POSTGRES_PORT}")
+            conn = psycopg2.connect(
+                dbname=POSTGRES_DB,
+                user=POSTGRES_USER,
+                password=POSTGRES_PASSWORD,
+                host=POSTGRES_HOST,
+                port=POSTGRES_PORT
+            )
+        
         conn.autocommit = True
         cur = conn.cursor()
 
@@ -45,7 +51,7 @@ def terminate_idle_connections():
         logging.info(f"Checked for idle connections: {num_idle_connections} found.")
 
         if num_idle_connections > 0:
-            logging.info(f"Idle connections found. Terminating connections...")
+            logging.info("Idle connections found. Terminating connections...")
             for pid, state, duration in idle_connections:
                 logging.info(f"Terminating idle connection with PID: {pid}, Duration: {duration}")
                 cur.execute(sql.SQL("SELECT pg_terminate_backend(%s);"), [pid])
